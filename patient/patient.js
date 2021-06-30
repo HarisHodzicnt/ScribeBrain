bf8adcc31217bdcb7cf59a716fdaaea72ed73d6e
HarisHodzicnt <haris_hodzicnt@hotmail.com>
2021-07-01 00:50:09 +0200

    one more feature in animation

--- patient/patient.js
+++ patient/patient.js
@@ -6,23 +6,35 @@ const handlePlay = () => {
     }
     document.getElementById('mic').src = '../assets/mic-green.png';
     document.getElementById('scribebrain-record').src = '../assets/scribebrain-recording.png';
-    addClass('show');
-    removeClass('hide');
+    addClass('bars', 'show');
+    removeClass('bars', 'hide');
+    addClass('circle', 'rotating');
 }
 
 const handleStop = () => {
     document.getElementById('mic').src = '../assets/mic-red.png';
     document.getElementById('scribebrain-record').src = '../assets/scribebrain-not-recording.png';
-    removeClass('show');
-    addClass('hide');
+    removeClass('bars','show');
+    removeClass('circle','rotating');
+    addClass('bars', 'hide');
 }
 
-const removeClass = (className) => {
-    document.getElementById('bars').classList.remove(className);
-    document.getElementById('bars2').classList.remove(className);
+const removeClass = (id, className) => {
+    if(id === 'bars') {
+        document.getElementById('bars').classList.remove(className);
+        document.getElementById('bars2').classList.remove(className);
+    }
+    else if (id === 'circle') {
+        document.getElementById('rotating-circle').classList.remove(className);
+    }
 }
 
-const addClass = (className) => {
-    document.getElementById('bars').classList.add(className);
-    document.getElementById('bars2').classList.add(className);
+const addClass = (id, className) => {
+    if(id === 'bars') {
+        document.getElementById('bars').classList.add(className);
+        document.getElementById('bars2').classList.add(className);
+    }
+    else if (id === 'circle') {
+        document.getElementById('rotating-circle').classList.add(className);
+    }
 }
