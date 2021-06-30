078841ffcb02cb3be67bef07618838d4ddf84f77
HarisHodzicnt <haris_hodzicnt@hotmail.com>
2021-07-01 00:16:23 +0200

    added hide show trigger

--- patient/patient.js
+++ patient/patient.js
@@ -6,10 +6,23 @@ const handlePlay = () => {
     }
     document.getElementById('mic').src = '../assets/mic-green.png';
     document.getElementById('scribebrain-record').src = '../assets/scribebrain-recording.png';
+    addClass('show');
+    removeClass('hide');
 }
 
 const handleStop = () => {
     document.getElementById('mic').src = '../assets/mic-red.png';
     document.getElementById('scribebrain-record').src = '../assets/scribebrain-not-recording.png';
+    removeClass('show');
+    addClass('hide');
 }
 
+const removeClass = (className) => {
+    document.getElementById('bars').classList.remove(className);
+    document.getElementById('bars2').classList.remove(className);
+}
+
+const addClass = (className) => {
+    document.getElementById('bars').classList.add(className);
+    document.getElementById('bars2').classList.add(className);
+}
